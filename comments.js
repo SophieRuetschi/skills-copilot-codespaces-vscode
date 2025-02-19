1f67d4ecc7610a3a99774f64e43a6e1ca8920e99
Sophie Ruetschi <sophie.ruetschi@ufl.edu>
2025-02-19 02:38:25 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,18 +1,37 @@
 // create web server
-// create a route that listens for "/comments" and returns a response with a list of comments
-// comments.js
+// 1. import express module
+// 2. create express app
+// 3. set up port
+// 4. create a route
+// 5. create a callback function
+// 6. listen to the port
+// 7. run the app
+
+// 1. import express module
 const express = require('express');
+
+// 2. create express app
 const app = express();
 
-app.get('/comments', (req, res) => {
-  res.json([
-    { username: 'Todd', comment: 'lolol' },
-    { username: 'Skyler', comment: 'lmao' },
-    { username: 'Sk8erBoi', comment: 'rofl' },
-    { username: 'Ada', comment: 'haha' },
-  ]);
-});
+// 3. set up port
+const port = 3000;
 
-app.listen(4001, () => {
-  console.log('Server is listening on port 4001');
-});
+// 4. create a route
+app.get('/', (req, res) => {
+    res.send('Hello World');
+})
+
+// 5. create a callback function
+app.get('/comments', (req, res) => {
+    res.send('This is comments page');
+})
+
+// 6. listen to the port
+app.listen(port, () => {
+    console.log(`Server is listening at http://localhost:${port}`);
+})
+
+// 7. run the app
+// node comments.js
+// go to browser and type http://localhost:3000
+// go to browser and type http://localhost:3000/comments
